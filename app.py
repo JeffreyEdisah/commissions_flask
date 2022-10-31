39c4b34b5b02f9cf142bd18416c939dba89533a8
Jeffrey Edisah <jeffreyedisah@gmail.com>
2022-10-31 14:53:34 +0100

    update form

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, request;
+from flask import Flask, flash, render_template, request, redirect, url_for;
 import re;
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route("/commissions")
+@app.route("/commissions",methods=["POST","GET"])
 def commissions():
     return render_template("commissions.html")
 
 mail_regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
 
-@app.route("/commissions_sent")
+@app.route("/commissions_sent",methods=["POST","GET"])
 def send_commissions():
-    if not re.fullmatch(mail_regex, request.form.get("mail")) or request.form.get("commissions") == "":
-        return render_template("failure.html")
+    mail = request.form.get("mail")
+    commissions = request.form.get("commissions")
+    if commissions == "":
+        flash("Pas de commissions envoyés")
+        return redirect(url_for('commissions'))
     return render_template("success.html")
